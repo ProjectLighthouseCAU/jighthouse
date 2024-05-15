c3bb27b8f579516996c05391902bcf77e223a7a3
Christopher <stu210088@mail.uni-kiel.de>
2024-05-15 13:25:14 +0200

    rename classes

---
+++ app/src/main/java/jighthouse/JhWebsockClient.java
@@ -0,0 +1,49 @@
+package jighthouse;
+
+import java.net.URI;
+import java.util.Map;
+//import java.net.URISyntaxException;
+import java.nio.ByteBuffer;
+
+import org.java_websocket.client.WebSocketClient;
+//import org.java_websocket.drafts.Draft;
+import org.java_websocket.drafts.Draft_6455;
+import org.java_websocket.handshake.ServerHandshake;
+
+public class JhWebsockClient extends WebSocketClient {
+
+	public JhWebsockClient(URI serverUri, Map<String, String> headers) {
+		super(serverUri, new Draft_6455(), headers, 0);
+	}
+
+	public JhWebsockClient(URI serverURI) {
+		super(serverURI);
+	}
+
+	@Override
+	public void onOpen(ServerHandshake handshakedata) {
+		// TODO: send("...");
+		System.out.println("new connection opened");
+	}
+
+	@Override
+	public void onClose(int code, String reason, boolean remote) {
+		System.out.println("closed with exit code " + code + " additional info: " + reason);
+	}
+
+	@Override
+	public void onMessage(String message) {
+		System.out.println("received message: " + message);
+	}
+
+	@Override
+	public void onMessage(ByteBuffer message) {
+		System.out.println("received ByteBuffer");
+	}
+
+	@Override
+	public void onError(Exception ex) {
+		System.err.println("an error occurred:" + ex);
+	}
+
+}

e5e060b3c3f6b8915c8133817acb0838ac194033
Christopher Widling <christopher.widling@gmail.com>
2024-06-12 23:32:50 +0200

    Proper byte 3D to 1D arr conversion method

--- app/src/main/java/jighthouse/JhFrameObject.java
+++ app/src/main/java/jighthouse/JhFrameObject.java
@@ -6,29 +6,63 @@ package jighthouse;
  */
 public class JhFrameObject {
     // ID flag
-    public int id;
+    private int id;
     // The actual image
-    // public int[][][] image;
     private byte[] buffer;
 
     /**
      * Constructor for a frame object.
      * @param img_id ID of frame
-     * @param img_data The actual image as int[][][]
+     * @param img_data The actual image as byte
+     */
+    public JhFrameObject(int img_id, byte[] img_data) {
+        this.id = img_id;
+        this.buffer = img_data;
+
+    }
+    /**
+     * Constructor for a frame object.
+     * @param img_id ID of frame
+     * @param img_data The actual image as byte[][][]
      */
     public JhFrameObject(int img_id, byte[][][] img_data) {
         this.id = img_id;
-        
+        this.buffer = from3DByteArray(img_data);
     }
 
-    private void encodePixel(byte[] nbuf, int pos, int n) {
-		int x = n%28;
-		int y = n/28;
-		buffer[pos + 3 * ((13-y)*28+x)] = (byte) 0xff;
-	}
+    /**
+     * Converts 3D to 1D Byte Array
+     * @param byteArray3D
+     * @return
+     */
+    private byte[] from3DByteArray(byte[][][] byteArray3D) {
+
+        int yLength = byteArray3D.length;
+        int xLength = byteArray3D[0].length;
+        int colorDepth = byteArray3D[0][0].length;
+
+        // resulting byte[] array has length (y * x * color)
+        byte[] byteArray = new byte[yLength * xLength * colorDepth];
+        int index = 0;
+
+        // Iterate through given 3D array
+        for (int y = 0; y < yLength; y++) {
+            for (int x = 0; x < xLength; x++) {
+                for (int c = 0; c < 3; c++) {
+                    // Write value into array
+                    byteArray[index++] = byteArray3D[y][x][Math.min(colorDepth, c)];
+                }
+            }
+        }
+        return byteArray;
+    }
 
     public byte[] getImage() {
         return buffer;
+    }
+
+    public int getId() {
+        return id;
     }
 
 }

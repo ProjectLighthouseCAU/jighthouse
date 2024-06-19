4e22d0438cb27b48963fd2de13540ba45c87d12e
Christopher Widling <christopher.widling@gmail.com>
2024-06-19 09:39:25 +0200

    refactor

--- lib/src/main/java/org/jighthouse/JhRequest.java
+++ lib/src/main/java/org/jighthouse/JhRequest.java
@@ -24,28 +24,26 @@ class JhRequest {
         this.PAYL = payload;
     }
 
+    /**
+     * Method that packs the request as a byte array using messagepack.
+     * 
+     * @return byte[] packed message
+     * @throws IOException
+     */
     public byte[] toByteArray() throws IOException {
-        MessageBufferPacker packer = MessagePack.newDefaultBufferPacker();
-        packer.packMapHeader(5);
-        packer.packString("REID");
-        packer.packInt(REID);
-        packer.packString("AUTH");
-        packer.packMapHeader(2);
-        packer.packString("USER");
-        packer.packString(USER);
-        packer.packString("TOKEN");
-        packer.packString(TOKEN);
-        packer.packString("VERB");
-        packer.packString("PUT");
-        packer.packString("PATH");
-        packer.packArrayHeader(3);
-        packer.packString("user");
-        packer.packString(USER);
-        packer.packString("model");
-        packer.packString("PAYL");
-        packer.packBinaryHeader(PAYL_SIZE);
-        packer.addPayload(PAYL, 0, PAYL_SIZE);
-        packer.close();
-        return packer.toByteArray();
-    }
+        try (MessageBufferPacker packer = MessagePack.newDefaultBufferPacker()) {
+            packer.packMapHeader(5)
+                  .packString("REID").packInt(REID)
+                  .packString("AUTH").packMapHeader(2)
+                      .packString("USER").packString(USER)
+                      .packString("TOKEN").packString(TOKEN)
+                  .packString("VERB").packString("PUT")
+                  .packString("PATH").packArrayHeader(3)
+                      .packString("user").packString(USER)
+                      .packString("model")
+                  .packString("PAYL").packBinaryHeader(PAYL_SIZE)
+                  .addPayload(PAYL, 0, PAYL_SIZE);
+            return packer.toByteArray();
+        }
+    }    
 }
